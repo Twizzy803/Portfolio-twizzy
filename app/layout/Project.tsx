4a19d8cead8d98410eac8ca663b42dcc9909ffd6
Twizzy <agilgilangcs77@gmail.com>
2026-02-12 16:38:00 +0700

    alert projek belom dibuat

--- app/layout/Project.tsx
+++ app/layout/Project.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+
+import ImgPort1 from "../assets/afterguilty/home.png";
 
 export default function Project() {
   return (
@@ -7,9 +10,11 @@ export default function Project() {
         <span className="bg-tertiary-me w-2 h-20 rounded-lg lg:my-5"> </span>
         <span className="bg-tertiary-me w-2 h-2 rounded-full"> </span>
       </section>
-      <section className="flex flex-col justify-center items-center m-5">
-        <div className="flex flex-row bg-tertiary-me w-2/3 h-50 rounded-xl">
-          <section className="w-1/2 p-4">
+      
+      {/* //* AfterGuilty Store */}
+      <a href="#" className="flex flex-col justify-center items-center m-5">
+        <div className="flex flex-row justify-center items-center bg-tertiary-me rounded-xl w-2/3">
+          <section className="pl-20 w-full">
             <div className="flex flex-row space-x-2">
               <p className="bg-secondary-me p-1 rounded-full text-primary-me">HTML</p>
               <p className="bg-secondary-me p-1 rounded-full text-primary-me">Bootstrap</p>
@@ -19,11 +24,30 @@ export default function Project() {
             <h1 className="text-4xl text-secondary-me font-bold">AfterGuilty.Store</h1>
             <p className="text-sm text-secondary-me my-4">Website yang berfokus pada jual beli produk fashion</p>
           </section>
-          <section className="w-1/2 p-4 items-center flex justify-center">
-            <img src="public/images/Photo.png" alt="" />
+          <section className="p-4">
+            <Image src={ImgPort1} alt="AfterGuilty Store" className="w-full rounded-lg" />
           </section>
         </div>
-      </section>
+      </a>
+
+      {/* //* Black Wooden */}
+      <a href="#" className="flex flex-col justify-center items-center m-5">
+        <div className="flex flex-row justify-center items-center bg-tertiary-me rounded-xl w-2/3">
+          <section className="pl-20 w-full">
+            <div className="flex flex-row space-x-2">
+              <p className="bg-secondary-me p-1 rounded-full text-primary-me">Flutter</p>
+              <p className="bg-secondary-me p-1 rounded-full text-primary-me">Dart</p>
+              <p className="bg-secondary-me p-1 rounded-full text-primary-me">MySQL</p>
+            </div>
+            <p className="text-xl text-secondary-me my-4">2023 | E-commerce | Website</p>
+            <h1 className="text-4xl text-secondary-me font-bold">Black Wooden</h1>
+            <p className="text-sm text-secondary-me my-4">Website yang berfokus pada jual beli produk fashion</p>
+          </section>
+          <section className="p-4">
+            <Image src={ImgPort1} alt="AfterGuilty Store" className="w-full rounded-lg" />
+          </section>
+        </div>
+      </a>
     </div>
   );
 }
